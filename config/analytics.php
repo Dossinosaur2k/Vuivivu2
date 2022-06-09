8d49cccfd544ea7bbe904d78aeb4c31ba994dd0a
Dossinosaur2k <dinhngoctrang1203@gmail.com>
2022-06-09 21:38:27 +0700

    fix_credential_to_deploy

--- config/analytics.php
+++ config/analytics.php
@@ -12,7 +12,7 @@ return [
      * to learn how to get this file. You can also pass the credentials as an array
      * instead of a file path.
      */
-    'service_account_credentials_json' => public_path('asset\analytics\vuivivu-8f3c03ddab32.json'),
+    'service_account_credentials_json' => public_path('analytics\vuivivu-8f3c03ddab32.json'),
 
     /*
      * The amount of minutes the Google API responses will be cached.

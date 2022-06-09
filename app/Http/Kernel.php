2e4f05b9633b7eb2ee3d51b5a43d9cb04d4552af
Dossinosaur2k <dinhngoctrang1203@gmail.com>
2022-06-09 21:43:03 +0700

    fix_kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check-role' => \App\Http\Middleware\CheckRole::class,
-        'check-status' => \App\Http\Middleware\CheckStatus::class,
+        'check-status' => \App\Http\Middleware\checkStatus::class,
     ];
 }
